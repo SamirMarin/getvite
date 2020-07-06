82d27bd33accc2ef76cfdc91c5f176fcb1a3028d
Samir Marin <marin.samir@gmail.com>
2020-07-05 21:17:50 -0700

    Add cockroachDB nodes to compose file Test connect to DB, make getvite DB

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ to start the app locally run:
 
 `docker-compose up -d`
 
+if new libraries where added to api run:
+
+`docker-compose up -d --build`
+
 to take a look at the logs:
 
 `docker-compose logs -f`
@@ -16,3 +20,7 @@ to take a look at the logs:
 to tear the containers down
 
 `docker-compose down`
+
+to tear down the database named volumes with containers
+
+`docker-compose down --volumes`
--- getvite_api/app.py
+++ getvite_api/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import psycopg2
+import psycopg2.errorcodes
+import logging
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'This is flask with docker after an update to app, test 3, reload cool'
+    dsn = 'postgresql://croach_node_1:26257/bank?sslmode=disable'
+    conn = psycopg2.connect(dsn)
+    with conn.cursor() as cur:
+        cur.execute('CREATE DATABASE getvite;')
+        logging.debug("create_db(): status message: {}".format(cur.statusmessage))
+    conn.commit()
+    return 'This is flask with docker after an update to app, test 3, reload cool, query to db check'
