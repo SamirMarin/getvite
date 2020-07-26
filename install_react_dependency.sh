b035491f9fb574dc9e68972f751f53ceacf498ca
Samir Marin <marin.samir@gmail.com>
2020-07-26 16:11:05 -0700

    Upgrade lodash andi websocket-extensions

--- install_react_dependency.sh
+++ install_react_dependency.sh
@@ -7,6 +7,11 @@ if [ -z "$dependecy" ]
 then
   echo "please call script with dependecy you like to add"
   echo "./install_react_dependency.sh {dependecy}"
+elif [ "$2" = "--upgrade" ]
+then
+  echo "upgrading $dependecy"
+  echo "running: yarn upgrade $dependecy, in docker container $container_name"
+  docker exec $container_name yarn upgrade $dependecy
 else
   echo "installing $dependecy"
   echo "running: yarn add $dependecy, in docker container $container_name"
